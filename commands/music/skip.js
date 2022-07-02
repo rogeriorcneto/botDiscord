0e6ee2974356787339af24b64bacd5ebdd269a40
rogeriorcneto <rogerioreiscassiano@hotmail.com>
2022-07-02 14:08:58 -0300

    commit inicial

---
+++ commands/music/skip.js
@@ -0,0 +1,23 @@
+module.exports = {
+  name: "skip",
+  aliases: ["s"],
+  utilisation: "{prefix}skip",
+  voiceChannel: true,
+
+  execute(client, message) {
+    const queue = player.getQueue(message.guild.id);
+
+    if (!queue || !queue.playing)
+      return message.channel.send(
+        `No music currently playing ${message.author}... try again ? ❌`
+      );
+
+    const success = queue.skip();
+
+    return message.channel.send(
+      success
+        ? `Current music ${queue.current.title} skipped ✅`
+        : `Something went wrong ${message.author}... try again ? ❌`
+    );
+  },
+};
